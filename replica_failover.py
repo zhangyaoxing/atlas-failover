50b5496caf3acc426f9191b0d8bafe96a4ed5edb
Yaoxing <yaoxing.zhang@gmail.com>
2018-04-25 17:19:51 +0800

    + add gitignore + detect primary node

--- replica_failover.py
+++ replica_failover.py
@@ -1,18 +1,32 @@
 from pymongo import MongoClient
 from faker import Faker
 import time
+import sys
 
 def write_test():
     fake = Faker()
     client = MongoClient("mongodb+srv://atlas:testAtlas@cluster1-o0ium.mongodb.net/test")
     db = client.get_database("failover")
+    adminDB = client.get_database("admin")
     collection = db.get_collection("failover")
     while True:
-        collection.insert_one({
-            "name": fake.name(),
-            "address": fake.address(),
-            "email": fake.email(),
-            "intro": fake.text()
-        })
-        print("1 document inserted!")
-        time.sleep(1)
+        try:
+            # Get replica set status
+            members = adminDB.command('replSetGetStatus')['members']
+            for m in members:
+                if m['stateStr'] == 'PRIMARY':
+                    print('Current primary: ' + m.name)
+
+            # Insert fake document
+            doc = {
+                "name": fake.name(),
+                "address": fake.address(),
+                "email": fake.email(),
+                "intro": fake.text()
+            }
+            collection.insert_one(doc)
+            print("1 document inserted! Email: " + doc['email'])
+        except:
+            print('Insert failed!', sys.exc_info()[0])
+        finally:
+            time.sleep(1)
